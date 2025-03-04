374cf6af3b2d2277d3d0346ca9c6f881841a665b
AleksyKuczynski <alehkuczynski@gmail.com>
2025-03-04 18:57:45 +0100

    Added aspect ratios to Greeting Card images

---
+++ src/data/greetingCardImages.ts
@@ -0,0 +1,328 @@
+
+export interface CardImage {
+  filename: string;
+  aspectRatio: number; // height/width
+}
+
+export interface CardSection {
+  images: CardImage[];
+  icons: string[];
+}
+
+
+// Auto-generated card sections with image aspect ratios
+export const cardSections: CardSection[] = [
+  {
+    "images": [
+      {
+        "filename": "01.1.webp",
+        "aspectRatio": 0.981675392670157
+      },
+      {
+        "filename": "01.2.webp",
+        "aspectRatio": 0.9973404255319149
+      },
+      {
+        "filename": "01.3.webp",
+        "aspectRatio": 0.8426966292134831
+      },
+      {
+        "filename": "01.4.webp",
+        "aspectRatio": 0.5
+      }
+    ],
+    "icons": [
+      "NY.4.svg",
+      "NY.1.svg",
+      "NY.2.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "02.1.webp",
+        "aspectRatio": 1.0822510822510822
+      },
+      {
+        "filename": "02.2.webp",
+        "aspectRatio": 0.6666666666666666
+      },
+      {
+        "filename": "02.3.webp",
+        "aspectRatio": 1.0869565217391304
+      },
+      {
+        "filename": "02.4.webp",
+        "aspectRatio": 0.38839979285344384
+      }
+    ],
+    "icons": [
+      "fest.4.svg",
+      "fest.1.svg",
+      "fest.2.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "03.1.webp",
+        "aspectRatio": 1.8796992481203008
+      },
+      {
+        "filename": "03.2.webp",
+        "aspectRatio": 1.0760401721664274
+      },
+      {
+        "filename": "03.3.webp",
+        "aspectRatio": 1.2647554806070826
+      },
+      {
+        "filename": "03.4.webp",
+        "aspectRatio": 1
+      }
+    ],
+    "icons": [
+      "SV.4.svg",
+      "SV.3.svg",
+      "SV.1.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "04.1.webp",
+        "aspectRatio": 1.0040160642570282
+      },
+      {
+        "filename": "04.2.webp",
+        "aspectRatio": 1.0302197802197801
+      },
+      {
+        "filename": "04.3.webp",
+        "aspectRatio": 0.4947229551451187
+      },
+      {
+        "filename": "04.4.webp",
+        "aspectRatio": 0.6666666666666666
+      }
+    ],
+    "icons": [
+      "NY.3.svg",
+      "NY.2.svg",
+      "NY.1.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "05.1.webp",
+        "aspectRatio": 1.2776831345826236
+      },
+      {
+        "filename": "05.2.webp",
+        "aspectRatio": 1.0387811634349031
+      },
+      {
+        "filename": "05.3.webp",
+        "aspectRatio": 0.6476683937823834
+      },
+      {
+        "filename": "05.4.webp",
+        "aspectRatio": 0.6666666666666666
+      },
+      {
+        "filename": "05.5.webp",
+        "aspectRatio": 0.8532423208191127
+      }
+    ],
+    "icons": [
+      "esther.2.svg",
+      "esther.5.svg",
+      "esther.4.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "06.1.webp",
+        "aspectRatio": 1.1627906976744187
+      },
+      {
+        "filename": "06.2.webp",
+        "aspectRatio": 0.962772785622593
+      },
+      {
+        "filename": "06.3.webp",
+        "aspectRatio": 0.5530973451327433
+      },
+      {
+        "filename": "06.4.webp",
+        "aspectRatio": 0.8571428571428571
+      }
+    ],
+    "icons": [
+      "esther.3.svg",
+      "esther.1.svg",
+      "esther.5.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "07.1.webp",
+        "aspectRatio": 1.1398176291793314
+      },
+      {
+        "filename": "07.2.webp",
+        "aspectRatio": 0.6014434643143545
+      },
+      {
+        "filename": "07.3.webp",
+        "aspectRatio": 1
+      },
+      {
+        "filename": "07.4.webp",
+        "aspectRatio": 1.5
+      }
+    ],
+    "icons": [
+      "NY.4.svg",
+      "NY.1.svg",
+      "NY.2.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "08.1.webp",
+        "aspectRatio": 0.9493670886075949
+      },
+      {
+        "filename": "08.2.webp",
+        "aspectRatio": 0.9986684420772304
+      },
+      {
+        "filename": "08.3.webp",
+        "aspectRatio": 0.7583417593528817
+      },
+      {
+        "filename": "08.4.webp",
+        "aspectRatio": 0.4832474226804124
+      }
+    ],
+    "icons": [
+      "NY.4.svg",
+      "NY.1.svg",
+      "NY.3.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "09.1.webp",
+        "aspectRatio": 0.89179548156956
+      },
+      {
+        "filename": "09.2.webp",
+        "aspectRatio": 0.9351620947630923
+      },
+      {
+        "filename": "09.3.webp",
+        "aspectRatio": 0.4606879606879607
+      },
+      {
+        "filename": "09.4.webp",
+        "aspectRatio": 0.7676560900716479
+      }
+    ],
+    "icons": [
+      "zodiac.4.svg",
+      "zodiac.1.svg",
+      "zodiac.2.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "10.1.webp",
+        "aspectRatio": 0.9157509157509157
+      },
+      {
+        "filename": "10.2.webp",
+        "aspectRatio": 0.5136986301369864
+      },
+      {
+        "filename": "10.3.webp",
+        "aspectRatio": 0.7403751233958539
+      }
+    ],
+    "icons": [
+      "fest.1.svg",
+      "fest.4.svg",
+      "fest.3.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "11.1.webp",
+        "aspectRatio": 1.1061946902654867
+      },
+      {
+        "filename": "11.2.webp",
+        "aspectRatio": 1.094890510948905
+      },
+      {
+        "filename": "11.3.webp",
+        "aspectRatio": 0.4355400696864111
+      },
+      {
+        "filename": "11.4.webp",
+        "aspectRatio": 0.7731958762886598
+      }
+    ],
+    "icons": [
+      "SV.4.svg",
+      "SV.3.svg",
+      "SV.2.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "12.1.webp",
+        "aspectRatio": 0.9191176470588235
+      },
+      {
+        "filename": "12.2.webp",
+        "aspectRatio": 0.97911227154047
+      },
+      {
+        "filename": "12.3.webp",
+        "aspectRatio": 0.7440476190476191
+      }
+    ],
+    "icons": [
+      "zodiac.1.svg",
+      "zodiac.4.svg",
+      "zodiac.5.svg"
+    ]
+  },
+  {
+    "images": [
+      {
+        "filename": "14.1.webp",
+        "aspectRatio": 1.0013351134846462
+      },
+      {
+        "filename": "14.2.webp",
+        "aspectRatio": 0.5985634477254589
+      }
+    ],
+    "icons": [
+      "NY.3.svg",
+      "NY.2.svg",
+      "NY.1.svg"
+    ]
+  }
+];
